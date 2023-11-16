948700afe8dd52166820b889adcdb06e47cb35a6
WillianPrates <71357338+WillianPrates@users.noreply.github.com>
2023-11-16 11:58:06 -0300

    Criação do projeto

---
+++ ConnectPet/ConnectPet/ConnectPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectPetApp.swift
+//  ConnectPet
+//
+//  Created by Foundastion03 on 16/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ConnectPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConnectPet/ConnectPet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ConnectPet
+//
+//  Created by Foundastion03 on 16/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
