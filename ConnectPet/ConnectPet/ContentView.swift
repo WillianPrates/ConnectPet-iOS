62ba1e789a5359222879a94a17e91fa18dcf950a
WillianPrates <71357338+WillianPrates@users.noreply.github.com>
2023-11-22 10:23:24 -0300

    Adicionada a funcao de adicionar e remover, juntamente com seus botoes e a funcao swipe
    Foi feito apenas a logica de adicionar e remover, porem esta configurado somente o nome do pet

--- ConnectPet/ConnectPet/ContentView.swift
+++ ConnectPet/ConnectPet/ContentView.swift
@@ -6,19 +6,83 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct ContentView: View {
+    
+//    var cachorroTeste: Pet = Pet(nomePet: "Leia", nascimento: "01/12/2021", peso: 10.2, raça: "SRD", vacinas: [], exames: [], consultas: [], vermifugos: [], foto: "Foto de cachorro", castrado: true, genero: "Femea")
+    @Environment(\.managedObjectContext) private var viewContext
+
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Pet.id, ascending: true)],
+        animation: .default)
+    private var pets: FetchedResults<Pet>
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(pets) { pet in
+                    NavigationLink {
+                        Text("Item at \(pet.nomePet!)")
+                    } label: {
+                        Text(pet.nomePet!)
+                    }
+                }
+                .onDelete(perform: deleteItems)
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    EditButton()
+                }
+                ToolbarItem {
+                    Button(action: addItem) {
+                        Label("Add Item", systemImage: "plus")
+                    }
+                }
+            }
+            Text("Select an item")
         }
-        .padding()
+    }
+
+    private func addItem() {
+        withAnimation {
+            let newPet = Pet(context: viewContext)
+            newPet.nomePet = "Teste 2"
+
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { pets[$0] }.forEach(viewContext.delete)
+
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
     }
 }
 
+private let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
 #Preview {
-    ContentView()
+    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
 }
