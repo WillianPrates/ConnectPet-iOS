eca5fcbdc0823ad6173976d21fd22ad86d9b5c73
WillianPrates <71357338+WillianPrates@users.noreply.github.com>
2023-11-24 11:30:43 -0300

    feat/Criado logica para salvar no banco cadastro dos pets

--- ConnectPet/ConnectPet/Flow/CadastroPet/CadastroPetViewModel.swift
+++ ConnectPet/ConnectPet/Flow/CadastroPet/CadastroPetViewModel.swift
@@ -6,12 +6,11 @@
 //
 
 import SwiftUI
+import CoreData
 
 class CadastroPetViewModel: ObservableObject {
     
     // MARK: - Fetch
-    
-    @Environment(\.managedObjectContext) private var viewContext
     
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Pet.id, ascending: true)],
@@ -20,17 +19,38 @@ class CadastroPetViewModel: ObservableObject {
     // MARK: - Publishers
     
     @Published var nomePet: String = ""
+    @Published var peso: Double = 0
+    @Published var dataNasc = Date()
+    @Published var castrado: Bool = true
+    @Published var especie: Especie = .cachorro
+    @Published var pelagem: Pelagem = .curta
+    @Published var sexo: Sexo = .femea
+    @Published var racaCachorro: RacaCachorro = .semRacaDefinida //fazer verificacao para salvar no banco
+    @Published var racaGato: RacaGato = .semRacaDefinida
     
     // MARK: - Functions
     
-    func addItem() {
+    func addItem(vc: NSManagedObjectContext) {
         withAnimation {
-            let newPet = Pet(context: viewContext)
+            let newPet = Pet(context: vc)
+            newPet.id = UUID()
             newPet.nomePet = nomePet
+            newPet.peso = peso
+            newPet.especie = String(especie.title)
+            newPet.sexo = sexo.title
+            newPet.pelagem = pelagem.title
+            newPet.castrado = castrado
             
+            if (newPet.especie == "cachorro"){
+                newPet.raca = racaCachorro.title
+            } else if (newPet.especie == "gato"){
+                newPet.raca = racaGato.title
+            } else {
+                newPet.raca = "Null"
+            }
 
             do {
-                try viewContext.save()
+                try vc.save()
             } catch {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
@@ -40,12 +60,12 @@ class CadastroPetViewModel: ObservableObject {
         }
     }
     
-    func deleteItems(offsets: IndexSet) {
+    func deleteItems(offsets: IndexSet, vc: NSManagedObjectContext) {
         withAnimation {
-            offsets.map { pets[$0] }.forEach(viewContext.delete)
+            offsets.map { pets[$0] }.forEach(vc.delete)
 
             do {
-                try viewContext.save()
+                try vc.save()
             } catch {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
