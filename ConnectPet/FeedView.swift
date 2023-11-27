7797c12de541b7a1bba16493b300f517ec85ff51
Augusto Leal <112337802+AugustoLealES@users.noreply.github.com>
2023-11-27 10:21:07 -0300

    feat/FeedView/Terminei o FeedView
    Terminando o feedView e organizando pastas

--- ConnectPet/FeedView.swift
+++ ConnectPet/FeedView.swift
@@ -1,7 +1,11 @@
 import SwiftUI
 
 struct FeedView: View {
+    @State private var selectedDate = Date()
+    @State private var isAnimated = false
+
     let corBackground = LinearGradient(gradient: Gradient(colors: [Color("Gradiente-Purple"), Color("Gradiente-Blue")]), startPoint: .leading, endPoint: .trailing)
+<<<<<<< Updated upstream
     
     var body: some View {
         NavigationStack {
@@ -20,10 +24,63 @@ struct FeedView: View {
                                         .bold()
                                 }
                                 HStack {
+=======
+    let buttonWidth: CGFloat = 150
+
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                corBackground
+                    .edgesIgnoringSafeArea(.all)
+
+                VStack {
+                    VStack {
+                        // Adicione o DatePicker acima dos botões
+                        DatePicker("Selecione uma data para consultar o histórico:", selection: $selectedDate, in: ...Date(), displayedComponents: .date)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .opacity(isAnimated ? 1 : 0) 
+
+                        HStack {
+                            Button(action: {
+                                print("Botão pressionado!")
+                            }, label: {
+                                VStack {
+                                    Text("Cartilha de Vacinação")
+                                        .lineLimit(2)
+                                        .foregroundColor(.black)
+                                        .bold()
+                                        .frame(width: buttonWidth)
+                                    Image(systemName: "syringe")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 30)
+                                        .foregroundColor(.blue)
+                                }
+                                .padding()
+                                .background(Color.white)
+                                .cornerRadius(10)
+                            })
+                            .opacity(isAnimated ? 1 : 0)
+
+                            Spacer()
+
+                            Button(action: {
+                                print("Botão pressionado!")
+                            }, label: {
+                                VStack {
+                                    Text("Histórico de Consultas")
+                                        .lineLimit(2)
+                                        .foregroundColor(.black)
+                                        .bold()
+                                        .frame(width: buttonWidth)
+>>>>>>> Stashed changes
                                     Image(systemName: "calendar")
                                         .resizable()
                                         .scaledToFit()
                                         .frame(width: 30)
+<<<<<<< Updated upstream
                                         .foregroundColor(.black)
                                 }
                             }
@@ -119,6 +176,82 @@ struct FeedView: View {
                     }
                 }
             }
+=======
+                                        .foregroundColor(.green)
+                                }
+                                .padding()
+                                .background(Color.white)
+                                .cornerRadius(10)
+                            })
+                            .opacity(isAnimated ? 1 : 0)
+                        }
+
+                        HStack {
+                            Button(action: {
+                                print("Botão pressionado!")
+                            }, label: {
+                                VStack {
+                                    Text("Histórico de Exames")
+                                        .lineLimit(2)
+                                        .foregroundColor(.black)
+                                        .bold()
+                                        .frame(width: buttonWidth)
+                                    Image(systemName: "pencil.and.list.clipboard")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 30)
+                                        .foregroundColor(.orange)
+                                }
+                                .padding()
+                                .background(Color.white)
+                                .cornerRadius(10)
+                            })
+                            .opacity(isAnimated ? 1 : 0)
+
+                            Spacer()
+
+                            Button(action: {
+                                print("Botão pressionado!")
+                            }, label: {
+                                VStack {
+                                    Text("Histórico de Medicações")
+                                        .lineLimit(2)
+                                        .foregroundColor(.black)
+                                        .bold()
+                                        .frame(width: buttonWidth)
+                                    Image(systemName: "pills")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 40)
+                                        .foregroundColor(.purple)
+                                }
+                                .padding()
+                                .background(Color.white)
+                                .cornerRadius(10)
+                            })
+                            .opacity(isAnimated ? 1 : 0)
+                        }
+                    }
+                    .padding()
+                    .onAppear {
+                        withAnimation(.easeIn(duration: 0.5)) {
+                            self.isAnimated = true
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Feed")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        // Ação que você deseja realizar
+                    }) {
+                        Image(systemName: "ellipsis.circle")
+                            .foregroundColor(.black)
+                    }
+                }
+            }
+>>>>>>> Stashed changes
         }
     }
 }
